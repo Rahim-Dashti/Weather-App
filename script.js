40a2ee5c04d736678aaa33677619c116adef47fb
Rahim Dashti <rahim.dashti.eng@gmail.com>
2023-05-26 16:02:51 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 async function get_weather() {
   let weather = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=d07e568ac6b24d47988152237232305&q=london"
+    "https://api.weatherapi.com/v1/current.json?key=d07e568ac6b24d47988152237232305&q=london",
+    { mode: "cors" }
   );
   let weather_info = weather.json();
   return weather_info;
